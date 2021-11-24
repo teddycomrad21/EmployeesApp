9eda7e53669a20a633dbd8e8535b926b214e0c0f
teddycomrad <teddycomrad21@gmail.com>
2021-11-24 12:53:54 +0200

    1 commit - paragraph addedto app-info

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -7,7 +7,7 @@ const AppInfo = ({ employeeAmount, increasedAmount }) => {
             <h1>Учёт сотрудников в компании GlobalLogic</h1>
             <h2>Общее число сотрудников: {employeeAmount}</h2>
             <h2>Премию получат: {increasedAmount}</h2>
-
+            <p>Изменение 1 для теста commit squash...</p>
         </div>
     );
 }
